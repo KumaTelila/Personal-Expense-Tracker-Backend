37e0282d7a783e75f6e198b6100692ec73909e1e
Kuma Telila <kumatelila26@gmail.com>
2024-07-20 10:58:14 +0300

    api test for server done

--- app.js
+++ app.js
@@ -1,18 +1,29 @@
 const express = require('express');
 const cors = require('cors');
 const {db} = require('./config/dbconfig');
+const {readdirSync} = require('fs');
 const app = express();
 
 require('dotenv').config();
 
 const PORT = process.env.PORT;
+const IP = process.env.IP || 'localhost';
 
-app.use(express.json);
+
+// middleware
+app.use(express.json());
 app.use(cors())
+
+//routes
+// app.use('/users', require('./routes/users'));
+app.use('/', require('./routes/transactions'));
+readdirSync('./routes').map((route) => app.use('/api', require('./routes/' + route)))
 
 const server = () => {
   db()
-  app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+  app.listen(PORT, IP);
+  console.log(`Server is running at http://${IP}:${PORT}`);
 }
 
 server()
+module.exports = app;
