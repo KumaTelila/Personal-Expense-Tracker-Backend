39265572c3f5a1b96f33dd656d9d6cba4797c07e
Kuma Telila <kumatelila26@gmail.com>
2024-07-20 13:44:46 +0300

    User registeration and login done

--- routes/transactions.js
+++ routes/transactions.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const { check } = require('express-validator');
 const { addExpense, getExpenses, getExpenseById, updateExpense, deleteExpense } = require('../controllers/expense');
+const { registerUser, loginUser } = require('../controllers/user');
 const router = express.Router();
-const validate = ()=>{
+const validateExpense = ()=>{
     return [
         check('title').not().isEmpty().withMessage('Title is required').isLength({ max: 50 }).withMessage('Title cannot be more than 50 characters'),
         check('amount').not().isEmpty().withMessage('Amount is required').isFloat({ min: 1 }).withMessage('Amount must be at least 1'),
@@ -11,12 +12,29 @@ const validate = ()=>{
         check('description').optional().isLength({ max: 255 }).withMessage('Description cannot be more than 255 characters')
     ]
 }
+const validateUserRegistration = () => {
+    return [
+        check('name').not().isEmpty().withMessage('Name is required').isLength({ max: 100 }).withMessage('Name cannot be more than 100 characters'),
+        check('email').isEmail().withMessage('Invalid email address').isLength({ max: 100 }).withMessage('Email cannot be more than 100 characters'),
+        check('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters').isLength({ max: 255 }).withMessage('Password cannot be more than 255 characters')
+    ]
+}
 
-router.post('/add-expense', validate(), addExpense);
+const validateUserLogin = () => {
+    return [
+        check('email').isEmail().withMessage('Invalid email address'),
+        check('password').exists().withMessage('Password is required')
+    ]
+}
+//user routes
+router.post('/register', validateUserRegistration(), registerUser);
+router.post('/login', validateUserLogin(), loginUser);
 
+// expense routes
+router.post('/add-expense', validateExpense(), addExpense);
 router.get('/expenses', getExpenses);
 router.get('/expenses/:id', getExpenseById);
-router.put('/expenses/:id', validate(), updateExpense);
+router.put('/update-expenses/:id', validateExpense(), updateExpense);
 router.delete('/expenses/:id', deleteExpense);
 
 module.exports = router;
